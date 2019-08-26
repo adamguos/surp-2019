bd2d371d290bf6d8063be2d32e320da67d5f5ead
Adam Guo <agsmguo@gmail.com>
2019-08-26 14:56:22 +0800

    Plot both direct compute and ifft

--- nfft-hermite/mehler/plot.py
+++ nfft-hermite/mehler/plot.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 
 t = np.loadtxt("t.mat")
 tn = np.loadtxt("tn.mat")
+tnl = np.loadtxt("tnl.mat")
 T = np.loadtxt("T.mat")
 A = np.loadtxt("A.mat")
 
-plt.plot(t, np.abs(tn))
+plt.plot(t, np.abs(tn), color="r", label="direct compute")
+plt.plot(t, np.abs(tnl), color="b", label="ifft")
+plt.legend()
 for i in range(len(T)):
 	plt.axvline(x=T[i], color="k", linestyle="--")
 	plt.text(T[i] + 0.02, 0, "t = {}\na = {}".format(T[i], A[i]))
-# plt.show()
 plt.savefig("plot.png")
