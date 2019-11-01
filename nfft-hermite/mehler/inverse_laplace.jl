c8247e9ff4ca993fa96c0b506820a2f0d0e117f5
Adam Guo <agsmguo@gmail.com>
2019-10-31 20:44:50 -0700

    Fix hermitefn.jl to work for both ifft_hermite.jl and inverse_laplace.jl

--- nfft-hermite/mehler/inverse_laplace.jl
+++ nfft-hermite/mehler/inverse_laplace.jl
@@ -6,6 +6,11 @@ include("hermitefn.jl")
 include("hfuncinfty.jl")
 include("observations.jl")
 
+function expmult(a, b)
+	s = sign(b)
+	return exp(log(a) + log(abs(b))) * s
+end
+
 # Set precision of BigFloat to 300 bits (~90 digits)
 setprecision(300)
 
@@ -15,17 +20,19 @@ samples = collect(range(-3, stop=3, length=npoints))
 quad_n = 128
 poly_n = quad_n ÷ 2
 w = (sqrt(5) - 1) / 2
-a = [1, 3, 2, 1]
+# a = [1, 3, 2, 1]
 a = [1, 3, 1]
-t = [-1, -0.25, 0.5, 1]
+# a = [1, 1, 1, 1, 1]
+# t = [-1, -0.25, 0.5, 1]
 t = [-1, 0, 0.75]
+# t = [-1, -0.5, 0, 0.5, 1]
 
-# Compute and normalise Gauss-Hermite nodes and weights
-# Convert Float64 output of gausshermite to BigFloat
+# Compute normalised Gauss-Hermite nodes and weights
 nodes, weights = FastGaussQuadrature.unweightedgausshermite(quad_n)
 
 obs = observations(a, t, nodes)
 hermites_nodes = hermitefn(poly_n, nodes)
+println(size(hermites_nodes))
 
 moments = zeros(poly_n)
 for k = 1:poly_n
@@ -34,7 +41,8 @@ for k = 1:poly_n
 		arr[l] = weights[l] * obs[l] * hermites_nodes[k, l]
 	end
 
-	moments[k] = w^(-k) * sum(arr)
+	# moments[k] = w^(-k) * sum(arr)
+	moments[k] = expmult(w^(-k), sum(arr))
 	print("moments: $k")
 end
 
@@ -50,5 +58,7 @@ for i = 1:npoints
 	print("kernelsum: $i")
 end
 
-plot(samples, kernelsum)
+plot!(samples, kernelsum)
+plot!(t, seriestype="vline")
 savefig("kernelsum.svg")
+plot()
